9d82e0e5395043ed7ff9e80aa4ba62f2d385074b
Harrison <Boyns12345@gmail.com>
2021-04-01 16:42:54 +0100

    Site map added

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_sitemap import Sitemap
 
 import os
 import config
